75bb7500235aee80d751da512b0acd97a85c1a80
Mason Salmon <Salmonmasons@aol.com>
2016-12-29 12:11:51 -0500

    Add files via upload

---
+++ Python If-Else.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    n = int(raw_input())
+if 6<n:
+    if 20>=n:
+        if n%2==0:
+            print("Weird")
+        else:
+            print("Not Weird")
+    else:    
+        if n%2==0:
+            print("Not Weird")
+        else:
+            print("Weird")
+else:    
+    if n%2==0:
+        print("Not Weird")
+    else:
+        print("Weird")
